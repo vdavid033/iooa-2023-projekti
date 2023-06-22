32b91676746359c700be0fcb2bc815a729b9d9e2
ivanagrbac <ivana.grbac3@gmail.com>
2023-06-22 21:41:24 +0200

    Početne postavke, početni izgled
    Implementacija quasar aplikacije, početne postavke.
    Početni izgled sučelja: navigacija, header, boje.

---
+++ vodjenje-projekta/src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Vođenje projekata
+        </q-toolbar-title>
+
+        <div>username</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Projekti',
+    icon: 'code',
+  },
+  {
+    title: 'Aktivnosti',
+    icon: 'plane',
+  },
+  {
+    title: 'Isporuke',
+    icon: 'task',
+  },
+  {
+    title: 'Izvješća',
+    icon: 'analytics',
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
